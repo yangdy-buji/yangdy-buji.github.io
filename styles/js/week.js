9787ac3f194b68aac990a6c7842d99aaabc0c847
不寂 <dongyu.ydy@alibaba-inc.com>
2019-02-14 14:29:06 +0800

    update

---
+++ styles/js/week.js
@@ -0,0 +1,42 @@
+/**
+ * 判断年份是否为润年
+ *
+ * @param {Number} year
+ */
+function isLeapYear(year) {
+    return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
+}
+/**
+ * 获取某一年份的某一月份的天数
+ *
+ * @param {Number} year
+ * @param {Number} month
+ */
+function getMonthDays(year, month) {
+    return [31, null, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31][month] || (isLeapYear(year) ? 29 : 28);
+}
+/**
+ * 获取某年的某天是第几周
+ * @returns {Number}
+ */
+function getWeekNumber() {
+    let now = new Date(),
+        year = now.getFullYear(),
+        month = now.getMonth(),
+        days = now.getDate();
+    //那一天是那一年中的第多少天
+    for (var i = 0; i < month; i++) {
+        days += getMonthDays(year, i);
+    }
+    //那一年第一天是星期几
+    let yearFirstDay = new Date(year, 0, 1).getDay() || 7;
+
+    let week = null;
+    if (yearFirstDay == 1) {
+        week = Math.ceil(days / yearFirstDay);
+    } else {
+        days -= (7 - yearFirstDay + 1);
+        week = Math.ceil(days / 7) + 1;
+    }
+    return "今天是"+year+"年的第"+week+"周";
+}
